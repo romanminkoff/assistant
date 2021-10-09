d35fb5703e7ea7b0a3f0907acf3454871e105a1c
rminkov <>
2021-10-09 15:22:56 +0200

    Keep, add new jobs

--- assistant.py
+++ assistant.py
@@ -1,19 +1,50 @@
 import os
 import sys
 
+import job
+
 EXCEPTIONS_LIMIT = 5
 INPUT_CHAR = "> "
 
+jobs = []
+
+def print_available_commands():
+    print(f"Available commands: {list(commands.keys())}")
+
 def cmd_help():
-    print("TODO: cmd_help")
+    print("Usage: enter command.")
+    print_available_commands()
 
 def cmd_exit():
     print("Have a nice day!")
     sys.exit(0)
 
+def cmd_list_jobs():
+    for i, job in enumerate(jobs):
+        print(f"  {i}:  {job.__dict__}")
+
+def _add_job(name, path, params, is_active):
+    j = job.Job(name, path, params, is_active)
+    jobs.append(j)
+
+def cmd_add_job():
+    name = input("  Name: ")
+    path = input("  Path: ")
+    params = input("  Params (opt): ") or None
+    is_active = input("  Is active? y/n: ")
+    is_active = True if is_active.lower() == "y" else False
+
+    is_input_correct = input("  Is input correct? Create this job? y/n: ")
+
+    if is_input_correct.lower() == "y":
+        _add_job(name, path, params, is_active)
+        print(f"  Job {name} was added.")
+
 commands = {
     "help": cmd_help,
-    "q": cmd_exit
+    "q": cmd_exit,
+    "jobs": cmd_list_jobs,
+    "add job": cmd_add_job,
 }
 
 def input_cmd():
@@ -42,17 +73,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-##### Tests
-
-__mock_input_string = ""
-def _mock_input():
-    return __mock_input_string
-
-
-#  - keep jobs
-#  - add jobs
-#  - show jobs
-#  - enable/disable jobs
-#  - show jobs status (upgrade prev.)
